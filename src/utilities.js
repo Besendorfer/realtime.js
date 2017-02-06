90ebd0273cfeca227d48241c7aa958af46a96748
Teancum Besendorfer <teancum.besendorfer@outlook.com>
2017-02-06 00:33:16 -0800

    Add more test cases for utilities, and add fix for isArray utility (test case found an issue).

--- src/utilities.js
+++ src/utilities.js
@@ -27,7 +27,7 @@
       return Object.prototype.toString.call(str) === '[object String]'
     },
     isArray: function (arr) {
-      return arr && arr.constructor === Array
+      return !!(arr && arr.constructor === Array)
     },
     isFunction: function(fn) {
       return !!(fn && fn.constructor && fn.call && fn.apply)
--- test/utilities-tests.js
+++ test/utilities-tests.js
@@ -26,3 +26,59 @@ describe('isString', () => {
     assert.equal(false, isString)
   })
 })
+
+describe('isArray', () => {
+  it('should determine if the given array parameter is an array', () => {
+    var isArray = Utilities.isArray(['string'])
+    assert.equal(true, isArray)
+  })
+
+  it('should determine if the given non-array parameters are not an array', () => {
+    var isArray = Utilities.isArray(1)
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray('string')
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray({ name: 'string' })
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray(() => {})
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray(true)
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray(false)
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray(null)
+    assert.equal(false, isArray)
+    isArray = Utilities.isArray(undefined)
+    assert.equal(false, isArray)
+  })
+})
+
+describe('isFunction', () => {
+  it('should determine if the given function parameter is a function', () => {
+    var isFunction = Utilities.isFunction(() => {})
+    assert.equal(true, isFunction)
+  })
+
+  it('should determine if the given non-function parameters are not a function', () => {
+    var isFunction = Utilities.isFunction(1)
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction(['string'])
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction({ name: 'string' })
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction('string')
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction(true)
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction(false)
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction(null)
+    assert.equal(false, isFunction)
+    isFunction = Utilities.isFunction(undefined)
+    assert.equal(false, isFunction)
+  })
+})
+
+describe('removeArrayItems', () => {
+  
+})
